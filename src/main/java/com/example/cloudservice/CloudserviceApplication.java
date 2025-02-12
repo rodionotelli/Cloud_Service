ea48db9f049756614fa8f9e2b77ce5906d80409e
rodionotelli <148154207+rodionotelli@users.noreply.github.com>
2025-02-12 17:30:26 +0300

    Update CloudserviceApplication.java
    Add logger

--- src/main/java/com/example/cloudservice/CloudserviceApplication.java
+++ src/main/java/com/example/cloudservice/CloudserviceApplication.java
@@ -1,13 +1,17 @@
 package com.example.cloudservice;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CloudserviceApplication {
-
+private static final Logger log = LogManager.getLogger(CloudserviceApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CloudserviceApplication.class, args);
+		log.info("App is running");
 	}
 
 }
